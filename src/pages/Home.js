f076761e5f3022df07c3bc6ad094b73a47ca9627
SHIJU G <shijuallinivad@gmail.com>
2023-02-25 16:45:35 +0530

    home image update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="relative   min-h-full w-full" style={{ height: "84vh" }}>
         <img
           className="h-full min-w-full"
-          src="https://images.unsplash.com/photo-1490682143684-14369e18dce8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zJTIwc3Vuc2V0JTIwbGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          src="https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
           alt=""
         />
         <div className="absolute top-0 left-0 w-full h-full bg-[#00000067] flex items-center justify-center">
@@ -21,7 +21,9 @@ const Home = () => {
             <p className=" text-xl">
               Rent the perfect van to make your perfect road trip
             </p>
-            <button className="text-white bg-yellow-500 w-fit px-10 py-3 font-bold text-2xl mt-8 rounded-lg">Find your van</button>
+            <button className="text-white bg-yellow-500 w-fit px-10 py-3 font-bold text-2xl mt-8 rounded-lg">
+              Find your van
+            </button>
           </div>
         </div>
       </div>
